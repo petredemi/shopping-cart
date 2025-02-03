01b1d8369e1ae398968bbf5de365347e3c7ab0bb
petrudemian <petredemi@gmail.com>
2025-02-03 09:58:35 +0000

    moved pictures in public directory

--- src/data.jsx
+++ src/data.jsx
@@ -3,25 +3,25 @@ const item1  = {
     idInput: 'pirite',
     name: 'Pirite Crystals',
     price: 60,
-    picture: 'src/images/pirita.jpg'
+    picture: 'pirita.jpg'
 }
 const item2 = {
     idInput: 'quart',
     name: 'Quartz Crystals',
     price: 80,
-    picture: 'src/images/cuart.jpg'
+    picture: 'cuart.jpg'
 }
 const item3 = {
     idInput: 'stibina',
     name: ' Stibnite Crystals',
     price: 120,
-    picture: 'src/images/stibina.jpg'
+    picture: 'stibina.jpg'
 }
 const item4 = {
     idInput: 'florina',
     name: 'Fluoride Crystals',
     price: 150,
-    picture: 'src/images/florina.jpg'
+    picture: 'florina.jpg'
 }
 
 
